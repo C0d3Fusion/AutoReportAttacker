29efe94f2b03ed455ced06a5a62cc0cab6eb5575
CodeFusion <innovationsnextgen250@gmail.com>
2024-12-26 00:14:43 +0530

    Create logo.py

---
+++ libs/logo.py
@@ -0,0 +1,12 @@
+# libs/logo.py
+
+from colorama import Fore
+
+def print_logo():
+    print(Fore.GREEN + """
+    ******************************************
+    *        Welcome to Attack Script        *
+    *        Proxy and Attack System         *
+    *          Script By Satyam Xd           *
+    ******************************************
+    """)
